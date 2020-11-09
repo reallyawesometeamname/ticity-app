fa8d97d15b8069a4b4cc7335c94124469acfa1d5
brianthomashammond <brianthomashammond@gmail.com>
2020-11-09 11:59:51 -0800

    bypassed authenticity token in application_controller_rb, edded controller methods ticity_searches_controller

--- app/controllers/ticity_searches_controller.rb
+++ app/controllers/ticity_searches_controller.rb
@@ -1,2 +1,39 @@
 class TicitySearchesController < ApplicationController
+  
+  def index
+    ticity_searches = TicitySearch.all
+    render json: ticity_searches
+  end
+
+  def create
+    ticity_search = TicitySearch.create(ticity_search_params)
+    if ticity_search.valid?
+      render json: ticity_search
+    else
+      render json: ticity_search.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    ticity_search = current_user.searches.find(params[:id])
+    ticity_search.update(ticity_search_params)
+    if ticity_search.valid?
+      render json: ticity_search
+    else
+      render json: ticity_search.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    ticity_search = current_user.searches.find(params[:id])
+    ticity_search.destroy()
+    render json: ticity_search
+  end
+
+  private
+
+  def ticity_search_params
+    params.require(:ticity_search).permit(:city1, :city2, :city3, :housing, :costofliving, :startups, :capital, :travel, :commute, :business, :safety, :healthcare, :education, :environment, :economy, :taxation, :internetaccess, :leisure, :tolerance, :outdoors, :user_id)
+  end
+
 end
