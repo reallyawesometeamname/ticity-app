d837fe9969996a10e8656b9a0e4356af158669ce
crgjr66 <chuckgilbreth@yahoo.com>
2020-11-17 14:55:19 -0800

    updated AboutUs testing

--- app/javascript/components/pages/__tests__/AboutUs.js
+++ app/javascript/components/pages/__tests__/AboutUs.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Enzyme, { mount, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-// import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import AboutUs from "../AboutUs.js";
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -14,16 +13,20 @@ it("About page renders without crashing", () => {
   ReactDOM.render(<AboutUs />, div);
 });
 
-it("About page renders content", () => {
-  const about = shallow(<AboutUs />);
-  expect(home.find("h3").text()).toEqual("Who is this Really Awesome Team?");
-});
-
 it("checks for first img tag and its properties", () => {
   const renderHome = shallow(<AboutUs />);
   expect(renderHome.find("img").first().props()).toEqual({
     alt: "Picture of Brian's adorable face",
     className: "about-pics",
-    src: "Brian.png",
+    src: "test-file-stub",
+  });
+});
+
+it("checks for third img tag and its properties", () => {
+  const renderHome = shallow(<AboutUs />);
+  expect(renderHome.find("img").last().props()).toEqual({
+    alt: "Picture of Matt's face looking downward",
+    className: "about-pics",
+    src: "test-file-stub",
   });
 });
